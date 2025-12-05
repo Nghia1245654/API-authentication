5bced7910b2dc5573f7d492ba16f189eeda4a2dc
Nghia1245654 <kingwinlata@gmail.com>
2025-12-06 00:00:59 +0700

    Add CORS configuration to enable cross-origin requests

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { swaggerDocs } from "./swagger.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
@@ -7,6 +8,13 @@ import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const app = express();
+
+// CORS configuration
+app.use(cors({
+  origin: true, // Allow all origins in development
+  credentials: true // Allow cookies to be sent
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 connectDB();
